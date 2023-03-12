b7e6d37cc3a87e893d316d2e3a849feeeeccd05d
hannes <hannesdelbeke@gmail.com>
2023-03-12 23:59:51 +0000

    add max test code

--- testcode.py
+++ testcode.py
@@ -1,6 +1,8 @@
-import sys
+# import sys
 # C:\Users\hanne\OneDrive\Documents\repos\pluginmanager
-sys.path.append("C:\\Users\\hanne\\OneDrive\\Documents\\repos\\plugget")
+# sys.path.append("C:\\Users\\hanne\\OneDrive\\Documents\\repos\\plugget")
+import site
+site.addsitedir("C:\\Users\\hanne\\OneDrive\\Documents\\repos\\plugget")
 
 import plugget
 import plugget.commands as cmd
@@ -12,3 +14,37 @@ reload(b)
 reload(cmd)
 
 cmd.install("textools")
+
+
+
+
+## ============================================
+
+#max
+# import sys
+#
+# # C:\Users\hanne\OneDrive\Documents\repos\pluginmanager
+# sys.path.append("C:\\Users\\hanne\\OneDrive\\Documents\\repos\\plugget")
+import site
+site.addsitedir("C:\\Users\\hanne\\OneDrive\\Documents\\repos\\plugget")
+
+import plugget
+import plugget.data as d
+import plugget.commands as cmd
+import plugget.actions.max_macroscript as ma
+# import plugget.apps.blender as b
+
+from importlib import reload
+
+reload(plugget)
+reload(d)
+reload(cmd)
+reload(ma)
+
+import traceback
+
+try:
+    cmd.install("uv-copy")
+except:
+
+    traceback.print_exc()
