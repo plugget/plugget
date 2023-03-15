07c79585367db8d697f9ad2beb5bdc25524a2771
hannes <hannesdelbeke@gmail.com>
2023-03-15 21:48:54 +0000

    fix blender addon installer

--- plugget/actions/blender_addon.py
+++ plugget/actions/blender_addon.py
@@ -14,6 +14,7 @@ def default_plugin_name(package_url, repo_url):
     else:
         return repo_url.rsplit("/", 1)[1].split(".")[0]
 
+
 def install(package: "plugget.data.Package", force=False, enable=True, **kwargs) -> bool:  # todo , force=False, enable=True):
     # If the “force” parameter is True, the add-on will be reinstalled, even if it has not been previously removed.
 
@@ -28,21 +29,28 @@ def install(package: "plugget.data.Package", force=False, enable=True, **kwargs)
     local_script_dir = bpy.utils.script_path_user()
     local_addons_dir = Path(local_script_dir) / "addons"
     new_plugin_path = local_addons_dir / plugin_path.name
-    if force:
-        from plugget.utils import rmdir
-        rmdir(new_plugin_path)
-    shutil.move(str(plugin_path), str(new_plugin_path.parent), )  # copy plugin_path to local_addons_dir
+    # if force:
+    #     from plugget.utils import rmdir
+    #     rmdir(new_plugin_path)
+    # shutil.move(str(plugin_path), str(new_plugin_path.parent), )  # copy plugin_path to local_addons_dir
+    # todo filter repo paths
+    for p in plugin_paths:
+        new_plugin_path.mkdir(parents=True, exist_ok=True)
+        shutil.move(str(p), str(new_plugin_path))
+
+    # todo clean up empty folders
 
     # check if plugin folder was copied, by checking if any files are in new_plugin_path
     if not any(new_plugin_path.iterdir()):
         logging.warning(f"Failed to install plugin {plugin_path.name}")
         return False
-    else:
-        return True
 
     if enable:
-        plugin_name = default_plugin_name(package.package_url, package.repo_url)
+        plugin_name = package.plugin_name or default_plugin_name(package.package_url, package.repo_url)
+        # print("PLUGGET enabling plugin_name ", plugin_name)
         bpy.ops.preferences.addon_enable(module=plugin_name)
+
+    return True
 
 
 def uninstall(package: "plugget.data.Package", **kwargs):
