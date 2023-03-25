02720252056c0905c3062a585b3fed61ff8d3b35
hannes <hannesdelbeke@gmail.com>
2023-03-25 23:54:42 +0000

    refactor temp file code

--- plugget/settings.py
+++ plugget/settings.py
@@ -1,12 +1,13 @@
 from pathlib import Path
 import os
+import tempfile
 
 
 sources = [
     "https://github.com/hannesdelbeke/plugget-pkgs.git",
 ]
 
-TEMP_PLUGGET = Path(os.getenv("TEMP")) / "plugget"
+TEMP_PLUGGET = Path(tempfile.gettempdir()) / "plugget"
 
 # plugget folder in user's appdata
 PLUGGET_DIR = Path(os.getenv("APPDATA")) / "plugget"
