60d19152483cf57e444f47f3fcfae569c5b25785
hannes <hannesdelbeke@gmail.com>
2023-04-15 11:21:02 +0100

    fix uninstall max, untested

--- plugget/actions/max_macroscript.py
+++ plugget/actions/max_macroscript.py
@@ -7,11 +7,11 @@ import shutil
 from pathlib import Path
 
 
-def install(package: "plugget.data.Package", **kwargs) -> bool:
-    # get macroscript folder
-    macro_folder_path = rt.getDir(rt.name('userMacros'))
+def install(package: "plugget.data.Package", max_folder=None, **kwargs) -> bool:
+    # get macroscript target folder
+    macro_folder_path = rt.getDir(rt.name('userMacros'))  # todo expose to kwarg
 
-    # move it to folder
+    # move it to target folder
     repo_paths = package.get_content()
     for sub_path in repo_paths:
         # sub_path = repo_path / sub_path
@@ -19,6 +19,7 @@ def install(package: "plugget.data.Package", **kwargs) -> bool:
         if sub_path.is_file() and sub_path.suffix != ".mcr":
             new_path = sub_path.with_suffix(".mcr")
             sub_path = sub_path.rename(new_path)
+
         # copy files (and folders) to the macroscript folder
         print("copying", sub_path, "to", macro_folder_path)
         shutil.copy(sub_path, macro_folder_path)
@@ -32,17 +33,31 @@ def uninstall(package: "plugget.data.Package", **kwargs):
     # get macroscript folder
     macro_folder_path = rt.getDir(rt.name('userMacros'))
 
-    for sub_path in package.repo_paths:
-        sub_path = Path(sub_path)
-        if sub_path.suffix != ".mcr":
-            sub_path = sub_path.with_suffix(".mcr")
-        # delete files (and folders) from the macroscript folder
-        sub_path = macro_folder_path / sub_path
-        print("deleting", sub_path, "from", macro_folder_path)
-        if sub_path.is_file():
-            sub_path.unlink()
+    # for sub_path in package.repo_paths:
+    #     sub_path = Path(sub_path)
+    #     if sub_path.suffix != ".mcr":
+    #         sub_path = sub_path.with_suffix(".mcr")
+    #     # delete files (and folders) from the macroscript folder
+    #     sub_path = macro_folder_path / sub_path
+    #     print("deleting", sub_path, "from", macro_folder_path)
+    #     if sub_path.is_file():
+    #         sub_path.unlink()
+    #     else:
+    #         sub_path.rmdir()
+
+
+    for p in package.installed_paths:
+        p = Path(p)
+
+        if p.suffix != ".mcr":
+            p = p.with_suffix(".mcr")
+
+        print("remove", p)
+        # delete all paths,. p can be folder or file. force delete and children
+        if p.is_dir():
+            shutil.rmtree(p, ignore_errors=True)
         else:
-            sub_path.rmdir()
+            p.unlink(missing_ok=True)
 
     # refresh macroscript folder to unload the uninstalled macros in max
     # pymxs.runtime.macros.load(macro_folder_path)
