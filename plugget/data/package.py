11abf683add67943e50d0e7b448064e0ee5b2cf4
hannes <hannesdelbeke@gmail.com>
2023-04-15 09:25:01 +0100

    warn instead of crash if installed

--- plugget/data/package.py
+++ plugget/data/package.py
@@ -270,7 +270,9 @@ class Package(object):
         from plugget import commands
 
         if self.is_installed and not force:
-            raise Exception(f"{self.package_name} is already installed")
+            logging.warning(f"{self.package_name} is already installed")
+            return
+            # raise Exception(f"{self.package_name} is already installed")
         for action in self.actions:
             # action install implicitly adds to self.install_paths
             action.install(self, *args, force=force, **kwargs)
