d352e06ec43c100802ec909902a10457fd9405b8
hannes <hannesdelbeke@gmail.com>
2023-05-22 22:56:40 +0100

    prep pythonpath so pip finds packages installed in e.g. addon folders

--- plugget/actions/blender_pip.py
+++ plugget/actions/blender_pip.py
@@ -3,6 +3,18 @@ import subprocess
 from pathlib import Path
 import bpy
 import importlib
+import os
+import sys
+
+
+def prep_pythonpath():
+    # copy the sys.paths to PYTHONPATH to pass to subprocess, for pip to use
+    paths = os.environ.get("PYTHONPATH", "").split(os.pathsep)
+    new_paths = [p for p in sys.path if p not in paths]
+    paths += new_paths
+    joined_paths = os.pathsep.join(paths)
+    if joined_paths:
+        os.environ["PYTHONPATH"] = joined_paths
 
 
 def get_requirements(package: "plugget.data.Package", **kwargs) -> list[Path]:
@@ -19,6 +31,8 @@ def get_requirements(package: "plugget.data.Package", **kwargs) -> list[Path]:
 
 # todo share pip functions
 def install(package: "plugget.data.Package", **kwargs):
+    prep_pythonpath()
+
     blender_user_site_packages = Path(str(bpy.utils.script_path_user())) / "modules"  # appdata
 
     for p in get_requirements(package):
@@ -41,6 +55,8 @@ def uninstall(package: "plugget.data.Package", dependencies=False, **kwargs):
 
     if not dependencies:
         return
+
+    prep_pythonpath()
 
     blender_user_site_packages = Path(str(bpy.utils.script_path_user())) / "modules"  # appdata
 
