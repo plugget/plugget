023564e053359d4d089e89de78c0c5a3522f7b9e
hannes <hannesdelbeke@gmail.com>
2023-11-27 12:17:58 +0000

    refactor blender requirements install action to class

---
+++ plugget/actions/_requirements.py
@@ -0,0 +1,38 @@
+"""
+Abstract Plugget action to pip install Python dependencies from requirements.txt.
+"""
+
+import plugget.actions._utils as action_utils
+import py_pip
+import os
+
+
+class RequirementsAction:
+    interpreter = None
+    target = None
+    env_var = None
+
+    @classmethod
+    def setup_py_pip(cls):
+        if not cls.interpreter:
+            cls.interpreter = os.environ.get(f"PLUGGET_{cls.env_var}_INTERPRETER")
+        if not cls.target:
+            cls.target = os.environ.get(f"PLUGGET_{cls.env_var}_TARGET_MODULES")
+        py_pip.default_target_path = cls.target
+        py_pip.python_interpreter = cls.interpreter
+
+    @classmethod
+    def install(cls, package: "plugget.data.Package", force=False, **kwargs):
+        cls.setup_py_pip()
+        for req_path in action_utils.iter_requirements_paths(package):
+            py_pip.install(requirements=req_path, force=force, upgrade=True)
+
+    @classmethod
+    def uninstall(cls, package: "plugget.data.Package", dependencies=False, **kwargs):
+        if not dependencies:
+            return
+        cls.setup_py_pip()
+        for req_path in action_utils.iter_requirements_paths(package):
+            print("requirements.txt found, uninstalling requirements")
+            print("package.clone_dir / p", package.clone_dir / req_path)
+            py_pip.uninstall(requirements=req_path, yes=True)
--- plugget/actions/blender_requirements.py
+++ plugget/actions/blender_requirements.py
@@ -1,50 +1,30 @@
 """
 Plugget action to install dependencies from requirements.txt using pip.
 
-PLUGGET_BLENDER_TARGET: env var to set target folder for pip install, if not set use user scripts folder from bpy
+PLUGGET_BLENDER_TARGET_MODULES: env var to set pip install target dir, else use blender user modules dir
 PLUGGET_BLENDER_INTERPRETER: env var to set python interpreter for pip install, defaults to sys.executable
 """
 
 from pathlib import Path
 import sys
-import plugget.actions._utils as action_utils
-import py_pip
+from plugget.actions._requirements import RequirementsAction
 import os
 
 
-def setup_py_pip():
-    py_pip.default_target_path = os.environ.get("PLUGGET_BLENDER_TARGET")
-    if not py_pip.default_target_path:
+def _get_modules_target_path():
+    default_target_path = os.environ.get("PLUGGET_BLENDER_TARGET_MODULES")
+    if not default_target_path:
         import bpy
-        py_pip.default_target_path = str(Path(str(bpy.utils.script_path_user())) / "modules")
-
-    # todo, startup script to add to site packages, else we cant support pth files.
-    py_pip.python_interpreter = os.environ.get("PLUGGET_BLENDER_INTERPRETER") or sys.executable
+        default_target_path = str(Path(str(bpy.utils.script_path_user())) / "modules")
+    return default_target_path
 
 
-def install(package: "plugget.data.Package", force=False, **kwargs):
-    setup_py_pip()
-    # TODO ideally use setup.py or pyproject.toml to install dependencies
-    # if requirements.txt exists in self.repo_paths, install requirements
-    for req_path in action_utils.iter_requirements_paths(package):
-        py_pip.install(requirements=req_path, force=force, upgrade=True)
+class _Action(RequirementsAction):
+    env_var = "BLENDER"
+    interpreter = sys.executable
+    target = _get_modules_target_path()
 
 
-def uninstall(package: "plugget.data.Package", dependencies=False, **kwargs):
-    # this method runs on uninstall, then the manifest is removed from installed packages
-    # ideally uninstall removes files from a folder,
-
-    if not dependencies:
-        return
-
-    setup_py_pip()
-
-    # uninstall plugget_qt
-    # we uninstall the dependencies (requirements)
-    # TODO is there a chance we share requirements with another package?
-    #  and uninstalling them breaks another package?
-
-    for req_path in action_utils.iter_requirements_paths(package):
-        print("requirements.txt found, uninstalling requirements")
-        print("package.clone_dir / p", package.clone_dir / req_path)
-        py_pip.uninstall(requirements=req_path, yes=True)
+install = _Action.install
+uninstall = _Action.uninstall
+__all__ = ["install", "uninstall"]
