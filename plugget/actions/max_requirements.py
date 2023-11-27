a75755a283718f89753ed035d0e5c3d4320d7fde
hannes <hannesdelbeke@gmail.com>
2023-11-27 15:23:04 +0000

    fix target path 3ds amx requir pip install

--- plugget/actions/max_requirements.py
+++ plugget/actions/max_requirements.py
@@ -6,18 +6,31 @@ import site
 from pathlib import Path
 import sys
 from plugget.actions._requirements import RequirementsAction
+import os
 
 
 def _get_interpreter_path():
+    # return "python"  # TODO hack since code below is buggy
     windows_python = Path(sys.executable).parent / "Python" / "python.exe"  # TODO support other OS
     return windows_python
+
+
+def get_site_packages():
+    # get env var APPDATA that contains 'C:\\Users\\hanne\\AppData\\Roaming\
+    # todo support other OS
+    roaming = os.environ.get("APPDATA")
+    for path in sys.path:
+        # get current python version in roaming
+        if path.startswith(roaming):
+            return path
+            # 'C:\\Users\\hanne\\AppData\\Roaming\\Python\\Python39\\site-packages'
+    # todo this path is not in site packages, might be a problem for pth files
 
 
 class _Action(RequirementsAction):
     env_var = "MAX"
     interpreter = _get_interpreter_path()
-    target = site.getsitepackages()[0]
-
+    target = get_site_packages()
 
 install = _Action.install
 uninstall = _Action.uninstall
