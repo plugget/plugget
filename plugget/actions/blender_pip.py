621a09cbbd26f446c25e52d33cd73033a20a7926
hannes <hannesdelbeke@gmail.com>
2023-11-27 09:34:02 +0000

    refactor blender pip

--- plugget/actions/blender_pip.py
+++ plugget/actions/blender_pip.py
@@ -1,16 +1,27 @@
+"""
+Plugget action to install dependencies from requirements.txt using pip.
+
+PLUGGET_BLENDER_TARGET: env var to set target folder for pip install
+PLUGGET_BLENDER_INTERPRETER: env var to set python interpreter for pip install
+"""
+
 from pathlib import Path
 import bpy
 import sys
 import plugget.actions._utils as action_utils
 import py_pip
+import os
 
 
-py_pip.default_target_path = str(Path(str(bpy.utils.script_path_user())) / "modules")  # defaults to no target path
-# todo, startup script to add to site packages, else we cant support pth files.
-py_pip.python_interpreter = sys.executable
+def setup_py_pip():
+    user_modules_folder = str(Path(str(bpy.utils.script_path_user())) / "modules")
+    py_pip.default_target_path = os.environ.get("PLUGGET_BLENDER_TARGET") or user_modules_folder
+    # todo, startup script to add to site packages, else we cant support pth files.
+    py_pip.python_interpreter = os.environ.get("PLUGGET_BLENDER_INTERPRETER") or sys.executable
 
 
 def install(package: "plugget.data.Package", force=False, **kwargs):
+    setup_py_pip()
     # TODO ideally use setup.py or pyproject.toml to install dependencies
     # if requirements.txt exists in self.repo_paths, install requirements
     for req_path in action_utils.iter_requirements_paths(package):
@@ -23,6 +34,8 @@ def uninstall(package: "plugget.data.Package", dependencies=False, **kwargs):
 
     if not dependencies:
         return
+
+    setup_py_pip()
 
     # uninstall plugget_qt
     # we uninstall the dependencies (requirements)
