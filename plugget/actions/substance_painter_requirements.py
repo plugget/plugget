c8f78da14bac0c5121757d711f9a3d87bb4b2ca6
hannes <hannesdelbeke@gmail.com>
2023-12-05 19:28:26 +0000

    hookup substance interpreter

--- plugget/actions/substance_painter_requirements.py
+++ plugget/actions/substance_painter_requirements.py
@@ -4,11 +4,17 @@ A Plugget action to pip install Python dependencies from requirements.txt for 3d
 
 from plugget.actions._requirements import RequirementsAction
 from plugget.actions._utils import get_my_documents
+from pathlib import Path
+import sys
+
+
+def get_interpreter_path():
+    return Path(sys.executable).parent / "resources" / "pythonsdk" / "python.exe"
 
 
 class _Action(RequirementsAction):
     env_var = "SUBSTANCE_PAINTER"
-    # interpreter = get_interpreter_path()  # TODO
+    interpreter = get_interpreter_path()
     target = get_my_documents() / "Adobe" / "Adobe Substance 3D Painter" / "python" / "modules"
 
 
