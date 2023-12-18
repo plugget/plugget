e539640a74954c5534da77efc665a82e8e294246
hannes <hannesdelbeke@gmail.com>
2023-12-18 18:34:37 +0000

    manually handle onedrive
    auto resolve didnt work in substance painter

--- plugget/actions/_utils.py
+++ plugget/actions/_utils.py
@@ -16,9 +16,9 @@ def try_except(func):
 def get_my_documents() -> Path:
     # todo support other OS
     user_dir = Path(os.environ.get("USERPROFILE"))  # e.g. 'C:\\Users\\hannes'
-    # Windows auto resolves the path to the ondrive folder
-    # one_drive_docs = user_dir / "OneDrive" / "Documents"
-    # if one_drive_docs.exists():
-    #     return one_drive_docs
-    # else:
-    return (user_dir / "Documents").resolve()
+    one_drive_docs = user_dir / "OneDrive" / "Documents"
+    if one_drive_docs.exists():
+        return one_drive_docs
+    else:
+        return (user_dir / "Documents").resolve()
+    # todo are there other locations for documents?
