a29abe13128de3c4a11df1d0942047ffd5fda527
hannes <hannesdelbeke@gmail.com>
2023-12-18 18:34:53 +0000

    enable substance plugin after install MVP

--- plugget/actions/substance_painter_plugin.py
+++ plugget/actions/substance_painter_plugin.py
@@ -1,5 +1,7 @@
 import plugget.actions._copy_files
 from plugget.actions._utils import get_my_documents
+from pathlib import Path
+import importlib
 
 
 class _Action(plugget.actions._copy_files.CopyFiles):
@@ -7,8 +9,22 @@ class _Action(plugget.actions._copy_files.CopyFiles):
 
     @classmethod
     def install(cls, package: "plugget.data.Package", **kwargs) -> bool:
-        super().install(package, **kwargs)
+        result = super().install(package, **kwargs)
 
+        # todo refresh plugins in substance painter
+        import substance_painter_plugins
+
+        # substance_painter_plugins.update_sys_path()
+
+        # Start the Plugin if it wasn't already: (.py name)
+        for plugin_path in package.installed_paths:
+            plugin_name = Path(plugin_path).stem
+
+            plugin = importlib.import_module(plugin_name)
+            if not substance_painter_plugins.is_plugin_started(plugin):
+                substance_painter_plugins.start_plugin(plugin_name)
+
+        return result
 
 install = _Action.install
 uninstall = _Action.uninstall
