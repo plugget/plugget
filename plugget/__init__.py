7f286b4bb84ec2399572e8b809f7df2591cac0ff
hannes <hannesdelbeke@gmail.com>
2024-05-20 21:43:07 +0100

    support pypip install custom modules

--- plugget/__init__.py
+++ plugget/__init__.py
@@ -4,4 +4,3 @@ Plugget is a plugin-manager for various applications.
 # from plugget import settings
 from plugget.commands import *
 from plugget.data import Package
-from plugget._utils import install_dependencies
--- plugget/_utils.py
+++ plugget/_utils.py
@@ -5,6 +5,9 @@ util functions for Plugget
 import os
 
 
+DEPENDENCIES = ["plugget", "py-pip", "detect-app", "requests"]
+
+
 def rmdir(path):
     # logging.debug(f"rmdir {path}")
     # delete folder on windows
@@ -13,20 +16,49 @@ def rmdir(path):
         os.system(f"rmdir /s /q {path}")
 
 
-def install_dependencies(app=None):
+def install_plugget_dependencies(app=None):
+    install_pypi(modules=DEPENDENCIES, app=app)
+
+
+def install_pypi(modules: "list[str]", app=None):
     """
     to simplify setup for non-technical users and avoid a complex pip install,
     users can copy-paste plugget in a pythonpath folder,
     then install the dependencies with the following code:
-        import plugget
-        plugget.install_dependencies(app="maya")
+
+    import plugget._utils
+    plugget._utils.install_dependencies(app="maya")
 
     this assumes the pip module is installed already
     """
     # todo freezes in maya 2026
-
     import plugget
 
     p = plugget.Package(app=app)  # create a fake package
     requirement_action = p.actions_args_kwargs[0][0]  # get the pip install action with target paths setup for the app
-    requirement_action.install(package=None, requirements=["py-pip", "detect-app", "requests"])  # install dependencies
+    requirement_action.install(package=None, requirements=modules)  # install dependencies
+    # todo this could be cleaner
+
+
+def update_plugget():
+    """
+    update plugget and its dependencies, e.g.:
+
+    import plugget._utils
+    plugget._utils.update_plugget()
+    """
+    import py_pip
+    import plugget
+    import detect_app
+
+    # set the correct target path and python interpreter for the app
+    # todo this could be cleaner
+    app = detect_app.detect_app()
+    p = plugget.Package(app=app)  # create a fake package
+    requirement_action = p.actions_args_kwargs[0][0]  # get the pip install action with target paths setup for the app
+    py_pip.default_target_path = requirement_action.target
+    py_pip.python_interpreter = requirement_action.interpreter
+
+    # upgrade plugget and its dependencies
+    for dep in DEPENDENCIES:
+        py_pip.install(package_name=dep, upgrade=True)
